0bb07bbc7caa7491521764f423fbaf852c251140
Tlapička <marek@tlapicka.net>
2012-11-15 09:13:28 +0100

    dokončení programu celsius.py. Čt 15.11.2012 09:13

--- celsius.py
+++ celsius.py
@@ -4,18 +4,23 @@
 # Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
 # Licence: GNU/GPL 
 # Úloha:   Program na převod °C -> °F
+################################################################
 
+start = input('od: ')
+stop  = input('do: ')
+krok = input('krok:')
 
-F=0.
-while F<=100 :
+F=start
+while F<=stop :
     C = 5.*(F-32)/9.
 #   print F, u'°F = ', C, u'°C'
     print u"{1:7.2f}°F = {0:7.2f}°C".format(C,F)
-    F = F+10
+    F = F+krok
     
 """
 http://docs.python.org/2.7/library/string.html#formatstrings
 """
+print 'Zkouším str.format()...'  
 print '###########################'  
 print 20/7
 print "|{0:<-20d}|".format(17)  # dekadické
