94b35d387fe69bcb21902b012b92d0b6895b9b30
Tlapička <marek@tlapicka.net>
2013-02-21 09:32:50 +0100

    obrázek, kružnice, Čt 21.02.2013 09:32:50

--- obrazek.py
+++ obrazek.py
@@ -14,14 +14,33 @@ import random
 w=400
 h=300
 
+#### vytvořím bílé plátno
 obr=[]
 # přídávám řádky
 for i in range(h):
     radek=[]
     # přídávám čísla do řádku
     for j in range(w):
-        radek.append( random.randint(0,100) )
+        radek.append( 100 )
     obr.append(radek)
+
+
+# projdu obrázek pixel po pixelu
+for y in range(h):
+    for x in range(w):
+        if x**2 + y**2 < 200**2 :
+            obr[y][x] = 0
+
+for y in range(h):
+    for x in range(w):
+        if (x-100)**2 + (y-200)**2 < 30**2 :
+            obr[y][x] = 50
+
+for y in range(h):
+    for x in range(w):
+       hodnota =(x-300)**2 + (y-100)**2  
+       if hodnota>= 80**2 and hodnota<=81**2:
+            obr[y][x] = 50
 
 lab.imshow(obr, interpolation='none', cmap='gray')
 
